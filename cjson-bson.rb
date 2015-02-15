7471c4009f4baccc867146b49c27d9915b59865c
David Thompson <dc@thompson.cx>
2015-02-14 19:36:15 -0500

    Behold the Kitware distribution mechanism!
    Dawdle in its brilliant gleem!

---
+++ cjson-bson.rb
@@ -0,0 +1,22 @@
+require "formula"
+
+class CJSON_BSON < Formula
+  homepage "http://github.com/vibraphone/cjson-bson"
+  head "https://github.com/vibraphone/cjson-bson.git"
+
+  depends_on"cmake" => :build
+
+  def install
+    args = std_cmake_args + %W[
+      -DBUILD_SHARED_LIBS=ON
+    ]
+
+    mkdir "build" do
+      args << ".."
+
+      system "cmake", *args
+      system "make"
+      system "make", "install"
+    end
+  end
+end
